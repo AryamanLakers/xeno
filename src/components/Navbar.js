cc258be183df3d72dba0c7efe982e499c1276b58
ARYAMAN <aryaman.kumar26@gmail.com>
2022-06-01 11:44:17 +0530

    design_updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,7 @@ export default function Navbar() {
     <div className="navbar_outer">
     	<div className="navbar">
         <a href="/"className="logo"><div id="inner_logo">Xeno</div></a>
-        <a href="/faq" className="common" id="FAQs">FAQs</a>
-        <a href="/privacy" className="common" id="Privacy_Policy">Privacy Policy</a>
-        <a href="/aml" className="common" id="AML">AML</a>
-        <a href="/t&c" className="common" id="T&C">T&C</a>
+        
 
         <button className="sign_up">Login</button>
         <button className="sign_in">Sign Up</button>
