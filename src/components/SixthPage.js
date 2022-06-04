df55aaced94f8ccff63831e067e6aba8b0c3e580
ARYAMAN <aryaman.kumar26@gmail.com>
2022-06-04 20:31:14 +0530

    design_updates

--- src/components/SixthPage.js
+++ src/components/SixthPage.js
@@ -3,7 +3,7 @@ import Button from "./Button.js";
 export default function SixthPage() {
   return (
   	<div className="investors">
-	      <div id="meta_data">Partnered By </div>
+	      <div id="meta_data">Partnered With </div>
 	      <div id="powered_buttons">
 	      	<div id="outer_image1"><div id="img1"></div></div>
 	      	<div id="outer_image2"><div id="img2"></div></div>
